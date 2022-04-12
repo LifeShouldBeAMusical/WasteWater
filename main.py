bfae45db3e25a2d0ac61c7bce6749592397b868a
RickiJay <rickipettinato@gmail.com>
2022-04-11 22:45:24 -0500

    County

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, pandas, datetime, sys, getopt
+import pandas, sys, getopt
 from ww_statistic import weighted_average
 from ww_data import get_data, get_most_recent_data
 
@@ -15,8 +15,8 @@ def main(argv: list[str]):
 	
 	most_recent_data = get_most_recent_data(data)
 	
-	percent_change_string = "% Change over 15 Days: {percent:.2f}%"
-	percent_peak_string = "% of Peak Levels: {percent:.2f}%"
+	percent_change_string = "% Change over 15 Days: {percent:.2f}% (Weighted Average)"
+	percent_peak_string = "% of Peak Levels: {percent:.2f}% (Weighted Average)"
 
 	percent_change = weighted_average(most_recent_data, 'ptc_15d', 'population_served')
 	percent_peak = weighted_average(most_recent_data, 'percentile', 'population_served')
@@ -35,13 +35,11 @@ def get_parsed_arguments(argv: list[str]) -> (str, str):
 	help_string = 'main.py -j <jurisdiction>'
 	
 	try:
-		opts, args = getopt.getopt(argv, "hj:", ["jurisdiction="])
+		opts, args = getopt.getopt(argv, "c:hj:", ["county=", "jurisdiction="])
 	except getopt.GetoptError:
 		print(help_string)
 		sys.exit(2)
 	for opt, arg in opts:
-		# print(opt)
-		# print(arg)
 		if opt == '-h':
 			print("Help")
 			print(help_string)
@@ -49,6 +47,10 @@ def get_parsed_arguments(argv: list[str]) -> (str, str):
 			param = "reporting_jurisdiction"
 			param_value = arg
 			print("Jurisdiction: %s" % param_value)
+		elif opt in ['-c', '--county']:
+			param = "county_names"
+			param_value = arg
+			print("County: %s" % param_value)
 	
 	return (param, param_value)
 
